82ca1efe96f95aab48b8065514fd068af179c283
ainzsoup <gamraouis@gmail.com>
2023-08-09 20:16:15 +0100

    state management is now handled by the Board component instead of each square

--- src/App.js
+++ src/App.js
@@ -1,39 +1,38 @@
 import React from 'react';
 import { useState } from 'react';
 
-function Square() {
-	const [value, setValue] = useState(null);
-	function handleClick() {
-		setValue('X');
-	}
+function Square({ value , onSquareClick}) {
   return (
 	<button 
-		className="square"
-		onClick={handleClick}
-		>
+		className="square" onClick={onSquareClick} >
 		{value}
 		</button>
   );
 }
 
 export default function Board() {
-
+	const [squares, setSquares] = useState(Array(9).fill(null));
+	function handleClick(i) {
+		const newSquares = squares.slice();
+		newSquares[i] = 'X';
+		setSquares(newSquares);
+	}
   return (
 	<React.Fragment>
 		<div className="board-row">
-			<Square />
-			<Square />
-			<Square />
+			<Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+			<Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+			<Square value={squares[2]} onSquareClick={() => handleClick(2)} />
 		</div>
 		<div className="board-row">
-			<Square />
-			<Square />
-			<Square />
+			<Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+			<Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+			<Square value={squares[5]} onSquareClick={() => handleClick(5)} />
 		</div>
 		<div className="board-row">
-			<Square />
-			<Square />
-			<Square />
+			<Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+			<Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+			<Square value={squares[8]} onSquareClick={() => handleClick(8)} />
 		</div>
 	</React.Fragment>
   );
