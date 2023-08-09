ebebd04bc8f640df1381c10a88c02483cee62327
ainzsoup <gamraouis@gmail.com>
2023-08-09 19:53:01 +0100

    empty board and squares turn into X when clicked

--- src/App.js
+++ src/App.js
@@ -1,5 +1,42 @@
 import React from 'react';
+import { useState } from 'react';
 
-export default function Square() {
-  return <button className="square">X</button>;
+function Square() {
+	const [value, setValue] = useState(null);
+	function handleClick() {
+		setValue('X');
+	}
+  return (
+	<button 
+		className="square"
+		onClick={handleClick}
+		>
+		{value}
+		</button>
+  );
 }
+
+export default function Board() {
+
+  return (
+	<React.Fragment>
+		<div className="board-row">
+			<Square />
+			<Square />
+			<Square />
+		</div>
+		<div className="board-row">
+			<Square />
+			<Square />
+			<Square />
+		</div>
+		<div className="board-row">
+			<Square />
+			<Square />
+			<Square />
+		</div>
+	</React.Fragment>
+  );
+}
+
+
